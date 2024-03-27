93b5d84f941eee68e6b0220f377609c7d8ee411f
Harsh Kandari <harshsingh7177@gmail.com>
2024-03-27 07:55:50 +0530

    Update sql_details.py

--- sql_details.py
+++ sql_details.py
@@ -4,7 +4,6 @@ import streamlit as st
 
 def conn_detail():
     try:
-        # Connect to MySQL database
         conn = mysql.connector.connect(
         host="cities.c58gyweuqusa.us-east-1.rds.amazonaws.com",
         user="admin",
@@ -24,32 +23,27 @@ def insert_detail_table(df, city):
     table_name = city.lower() 
     cursor, connection = conn_detail()
 
-    try:
-        for index, row in df.iterrows():
+
+    for index, row in df.iterrows():
             sql = f"INSERT INTO `{table_name}` (BHK, Status, Size_Sq_ft, Price_Sqft, Price, bath, area, project) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-            # Convert each value to the appropriate type (e.g., str to int)
-            values = (
-                int(row['BHK']) if not pd.isna(row['BHK']) else None,
-                str(row['Status']),
-                float(row['Size(Sq/ft)']) if not pd.isna(row['Size(Sq/ft)']) else None,
-                int(row['Price/Sqft']) if not pd.isna(row['Price/Sqft']) else None,
-                float(row['Price']) if not pd.isna(row['Price']) else None,
-                int(row['bath']) if not pd.isna(row['bath']) else None,
-                str(row['area']),
-                str(row['project'])
-            )
-            print("Executing SQL query:", sql)  # Print SQL query for debugging
-            print("Values:", values)
+            try:
+                values = (
+                    int(row['BHK']) ,
+                    str(row['Status']),
+                    float(row['Size_Sq_ft']) ,
+                    int(row['Price_Sqft']),
+                    float(row['Price']),
+                    int(row['bath']) ,
+                    str(row['area']),
+                    str(row['project'])
+                )
+            except:
+                continue
             cursor.execute(sql, values)
         
-        connection.commit()
-
-    except mysql.connector.Error as error:
-        print(f"Error inserting data: {error}")
-
-    finally:
-        cursor.close()
-        connection.close()
+            connection.commit()
+    cursor.close()
+    connection.close()
 
 def create_detail(suburb):
     cursor, con = conn_detail()
@@ -77,21 +71,14 @@ def detail_tables():
     cursor,con=conn_detail()
     try:
         sql_query = "SHOW TABLES"
-
-        # Execute the SQL query
         cursor.execute(sql_query)
-
-        # Fetch all rows from the result set
         rows = cursor.fetchall()
-
-        # Extract table names from the result set
         table = [row[0] for row in rows]
         return table
-
     finally:
         cursor.close()
         con.close()
-@st.cache_data
+
 def get_detail_data(table_name):
     cursor,con=conn_detail()
     try:
