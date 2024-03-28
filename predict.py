5cfa2e77c944bbf1384b275b836455bfda9cd302
Harsh Kandari <harshsingh7177@gmail.com>
2024-03-29 00:24:06 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -63,11 +63,11 @@ def predict_page(suburb):
         st.success("Inputs submitted successfully!")
         prediction, errors = predict_house_price(df, [bhk, size_sq_ft, bath, ready_to_move, under_construction])
         if price_in_rupees >= 10000000:
-            price_in_crore = price_in_rupees / 10000000
+            price_in_crore = prediction / 10000000
             price=price_in_crore+" Cr"
         else:
-            price_in_lakh = price_in_rupees / 100000
-            pprice=price_in_crore+" Lakh"
+            price_in_lakh = prediction/ 100000
+            price=price_in_lakh+" Lakh"
         st.write(f"Predicted Value: {price}")
         scaled_prediction = prediction / max_value  # Scale prediction to range [0.0, 1.0]
         progress_bar = st.progress(scaled_prediction)
